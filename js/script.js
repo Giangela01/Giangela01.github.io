110fe7140f400b7e9f0bac483143655151b4edeb
Gianelle Tianco <gianelle.tianco@gmail.com>
2021-08-30 01:21:20 -0400

    Fixing BTN

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+$('#s-word').on('input', function(event) {
+    let searchBtn = document.createElement("button");
+    searchBtn.innerHTML = "?";
+    searchBtn.type = "submit";
+    searchBtn.setAttribute("id", "s-btn");
+    document.getElementById("search").appendChild(searchBtn)
+
+});
