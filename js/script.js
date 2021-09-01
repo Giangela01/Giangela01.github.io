e9a8ed335627e402cf934781bad93cd8c72e01a8
Gianelle Tianco <gianelle.tianco@gmail.com>
2021-08-31 22:11:57 -0400

    Added Functions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,47 @@
+//for button to appear
 
-
-$('#s-word').on('input', function(event) {
+$('#s-word').change(function() {
     let searchBtn = document.createElement("button");
     searchBtn.innerHTML = "?";
     searchBtn.type = "submit";
     searchBtn.setAttribute("id", "s-btn");
     document.getElementById("search").appendChild(searchBtn)
 });
+
+// $('form').on('submit', getInfo);
+
+function getInfo(event) {
+    // event.preventDefault();
+
+    $.ajax({
+         url:'https://api.dictionaryapi.dev/api/v2/entries/en/delta'
+      }).then(
+        (data) => {
+         wordData = data;
+         retrieveInfo();
+        console.log(wordData);
+        },
+        (error) => {
+         console.log('bad request', error);
+        }
+    );    
+}
+
+function retrieveInfo(data) {
+    let table = document.getElementById('myTable')
+
+    for (let i = 0; i < wordData.length; i++) {
+        let row = ` <tr>
+                        <td>${wordData[i].word}</td>
+                    </tr>
+                    <tr>
+                        <td>${wordData[i].origin}</td>
+                    </tr>
+                    <tr>
+                        <td>${wordData[i].meanings}</td>
+                    </tr>`
+        table.innerHTML += row
+
+
+    }
+}
