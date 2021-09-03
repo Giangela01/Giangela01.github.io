96a08ec06d0700917e8bba5694ed3fc20edad992
Gianelle Tianco <gianelle.tianco@gmail.com>
2021-09-02 22:15:35 -0400

    HTML Skeletal Form

--- js/script.js
+++ js/script.js
@@ -18,26 +18,25 @@ function getInfo(event) {
     );
 }
 
-function retrieveInfo(data) {
-    let table = document.getElementById('myTable')
+// function retrieveInfo(data) {
+//     let table = document.getElementById('myTable')
 
-    for (let i = 0; i < wordData.length; i++) {
+//     for (let i = 0; i < wordData.length; i++) {
 
-        let row = `<tr>
-                        <td>${wordData[i].phonetic}</td>
-                    </tr>
-                    <tr>
-                        <td>${wordData[i].origin}</td>
-                    </tr>`
-        for (let e = 0; e < wordData[i].meanings.length; e++) {
-            let otherRow = ` <tr>
-                            <td>${wordData[i].meanings[e].partOfSpeech}</td>
-                        </tr>`
-            table.innerHTML += row
+//         let row = `<tr>
+//                         <td>${wordData[i].phonetic}</td>
+//                     </tr>
+//                     <tr>
+//                         <td>${wordData[i].origin}</td>
+//                     </tr>`
+//         for (let e = 0; e < wordData[i].meanings.length; e++) {
+//             let otherRow = ` <tr>
+//                             <td>${wordData[i].meanings[e].partOfSpeech}</td>
+//                         </tr>`
+//             table.innerHTML += row
 
-            table.innerHTML += otherRow
-            console.log('Hello');
-        }
-
-    }
-}
+//             table.innerHTML += otherRow
+//             console.log('Hello');
+//         }
+//     }
+// }
