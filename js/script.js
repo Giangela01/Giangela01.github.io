bb0d0fa369f9335eb8bb7bccd69bbe0d598e3319
Gianelle Tianco <gianelle.tianco@gmail.com>
2021-09-03 00:47:54 -0400

    JS Update Definitions track

--- js/script.js
+++ js/script.js
@@ -9,7 +9,9 @@ function getInfo(event) {
     }).then(
         (data) => {
             wordData = data;
-            retrieveInfo();
+            // retrieveInfo();
+            getMainWord();
+            getPoS();
             console.log(wordData);
         },
         (error) => {
@@ -18,25 +20,39 @@ function getInfo(event) {
     );
 }
 
-// function retrieveInfo(data) {
-//     let table = document.getElementById('myTable')
 
-//     for (let i = 0; i < wordData.length; i++) {
 
-//         let row = `<tr>
-//                         <td>${wordData[i].phonetic}</td>
-//                     </tr>
-//                     <tr>
-//                         <td>${wordData[i].origin}</td>
-//                     </tr>`
-//         for (let e = 0; e < wordData[i].meanings.length; e++) {
-//             let otherRow = ` <tr>
-//                             <td>${wordData[i].meanings[e].partOfSpeech}</td>
-//                         </tr>`
-//             table.innerHTML += row
+function getMainWord(data) {
+    document.getElementById("search-word").innerHTML =
+        `<h2 class="main-word">${wordData[0].word}</h2>
+    <audio controls src="${wordData[0].phonetics[0].audio}" type="audio/mp3">
+    Sorry, your browser does not seem support the audio.</audio>`
+}
 
-//             table.innerHTML += otherRow
-//             console.log('Hello');
-//         }
-//     }
-// }
+function getPoS(data) {
+    let table = document.getElementById('pos-phonetics')
+
+    for (let a = 0; a < wordData.length; a++) {
+        for (let b = 0; b < wordData[a].meanings.length; b++) {
+
+            for (let c = 0; c < wordData[a].meanings[b].definitions.length; c++) {
+
+
+                function definition(definition) {
+                    return `<dl>${definition}`}
+            
+
+
+                let row = `<p>${wordData[a].meanings[b].partOfSpeech} | ${wordData[a].phonetic}</p>
+                <p>${wordData[a].origin}</p>
+                <div id="def-example">${wordData[a].meanings[b].definitions[c].definition ? definition(wordData[a].meanings[b].definitions[c].definition) : "" }</div>`;
+
+                
+                // let defRow = `<dl><dt>${wordData[a].meanings[b].definitions[c].definition}</dt>
+                // <dt>${wordData[a].meanings[b].definitions[c].example}</dt></dl>`
+                table.innerHTML += row
+            }
+            // table.innerHTML += row
+        }
+    }
+}
