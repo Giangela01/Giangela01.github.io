d67990b8cb63b04c6cf203da56461d41f7a6a3d9
Gianelle Tianco <gianelle.tianco@gmail.com>
2021-09-04 18:15:49 -0400

    Fixed js function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 let wordData;
 
+const $input = $('input[type="text"]');
+
 $('form').on('submit', getInfo);
 
 function getInfo(event) {
@@ -7,14 +9,12 @@ function getInfo(event) {
     event.preventDefault();
 
     $.ajax({
-        url: 'https://api.dictionaryapi.dev/api/v2/entries/en/red'
+        url: `https://api.dictionaryapi.dev/api/v2/entries/en/${$input.val()}`
     }).then(
         (data) => {
             wordData = data;
-            // retrieveInfo();
             getMainWord();
             getPoS();
-            // console.log(wordData);
         },
         (error) => {
             console.log('bad request', error);
@@ -26,21 +26,28 @@ function getMainWord(data) {
     document.getElementById("search-word").innerHTML =
         `<h2 class="main-word">${wordData[0].word}</h2>
     <audio controls src="${wordData[0].phonetics[0].audio}" type="audio/mp3">
-    Sorry, your browser does not seem support the audio.</audio>`
+    Sorry, your browser does not seem support the audio.</audio>`;
+
 }
 
 function getPoS(data) {
 
     let table = document.getElementById('pos-phonetics');
+    let defTitle = document.getElementById('def-title');
     let defTable = document.getElementById('definition');
     let meanings = wordData[0].meanings[0].definitions;
+
+    defTitle.innerText = "Definition/s"
 
     let partOfSpeech = `<p>${wordData[0].meanings[0].partOfSpeech} | ${wordData[0].phonetic}</p>
     <p><em>Origin:</em> ${wordData[0].origin}</p>`;
 
     meanings.forEach(element => {
-       let defA= element.definition
-       let defList = defA.map = (`<p>${defA}</p>`);
+        let defA = element;
+        let defList = defA.map = (`<dl><lh>:${defA.definition}</lh>
+                                    <dt><em>${defA.example ? 'Example: ' + defA.example : ""}</em></dt>
+                                    <dd>${defA.synonyms ? defA.synonyms : ""}</dd>
+                                    </dl>`);
         defTable.innerHTML += defList;
     });
 
